a16c10d5c206d524c6606753db8dacbc36b25250
Amine Fezzani <100478662+Za3tour420@users.noreply.github.com>
2024-05-19 14:38:53 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from user import *
 
 if __name__ == "__main__":
+    #WINDOWS AUTHENTICATION
     db = DatabaseManager(server="YOUR_SERVER", database="YOUR_DATABASE")
+    
+    #USER AUTHENTICATION (COMMENT THE LINE ABOVE AND UNCOMMENT THE LINE BELOW)
+    #db = DatabaseManager(server="YOUR_SERVER", database="YOUR_DATABASE", trusted_connection="no", uid="YOUR_USERNAME", pwd="YOUR_PASSWORD")
+    
     print("Connecting to database...")
     connectionManager = db.connectToDatabase()
     if connectionManager:
